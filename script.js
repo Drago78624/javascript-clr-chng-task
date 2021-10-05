452810f8a349cc19588bf9fc542bbcac42792215
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2021-10-05 20:16:38 +0500

    js clr chng task

---
+++ script.js
@@ -0,0 +1,71 @@
+// setInterval(function func_1(){
+//     let color = "color_2"
+//     document.querySelector(".item-1").classList.replace("color_1",color) 
+// }, 1000)
+// setInterval(function func_2(){
+//     let color = "color_3"
+//     document.querySelector(".item-1, .item-2").classList.replace("color_2",color)        
+// }, 2000)
+// setInterval(function func_3(){
+//     let color = "color_4"
+//     document.querySelector(".item-1, .item-3, .item-2").classList.replace("color_3",color)
+// }, 3000)
+// setInterval(function func_4(){
+//     let color = "color_1"
+//     document.querySelector(".item-1, .item-2, .item-3, .item-4").classList.replace("color_4", color)
+// }, 4000)
+setInterval(function(){
+    document.querySelectors(".item-1").style.backgroundColor = "red"
+}, 1000)
+setInterval(function(){
+    document.querySelector(".item-1").style.backgroundColor = "green"
+}, 2000)
+setInterval(function(){
+    document.querySelector(".item-1").style.backgroundColor = "yellow"
+}, 3000)
+setInterval(function(){
+    document.querySelector(".item-1").style.backgroundColor = "darkgreen"
+}, 4000)
+
+setInterval(function(){
+    document.querySelectors(".item-2").style.backgroundColor = "orange"
+}, 1000)
+setInterval(function(){
+    document.querySelector(".item-2").style.backgroundColor = "lightblue"
+}, 2000)
+setInterval(function(){
+    document.querySelector(".item-2").style.backgroundColor = "black"
+}, 3000)
+setInterval(function(){
+    document.querySelector(".item-2").style.backgroundColor = "brown"
+}, 4000)
+
+setInterval(function(){
+    document.querySelectors(".item-3").style.backgroundColor = "darkcyan"
+}, 1000)
+setInterval(function(){
+    document.querySelector(".item-3").style.backgroundColor = "salmon"
+}, 2000)
+setInterval(function(){
+    document.querySelector(".item-3").style.backgroundColor = "pink"
+}, 3000)
+setInterval(function(){
+    document.querySelector(".item-3").style.backgroundColor = "violet"
+}, 4000)
+
+setInterval(function(){
+    document.querySelectors(".item-4").style.backgroundColor = "seagreen"
+}, 1000)
+setInterval(function(){
+    document.querySelector(".item-4").style.backgroundColor = "darkorchid"
+}, 2000)
+setInterval(function(){
+    document.querySelector(".item-4").style.backgroundColor = "bisque"
+}, 3000)
+setInterval(function(){
+    document.querySelector(".item-4").style.backgroundColor = "lavender"
+}, 4000)
+
+
+
+
